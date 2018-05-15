484fbccf0bccb178960880c9d333c23cf6385721
Qpadev <alessio.fino@mail.polimi.it>
2018-05-15 11:43:55 +0200

    Scritto javadoc classe die e cell, generata documentazione

--- src/main/java/it/polimi/se2018/model/cell/Cell.java
+++ src/main/java/it/polimi/se2018/model/cell/Cell.java
@@ -5,19 +5,35 @@ import it.polimi.se2018.utils.exceptions.NoDieException;
 
 import java.io.Serializable;
 
+/**
+ * Classe cella della plancia
+ * @author Giampietro
+ */
 public class Cell implements Serializable {
     private final Restriction restriction;
     private Die die;
     private boolean isUsed = false;
 
+    /**
+     * Costruttore
+     * @param restriction restrizione da applicare alla cella
+     */
     public Cell(Restriction restriction) {
         this.restriction = restriction;
     }
 
+    /**
+     * Ritorna se nella cella c'e' un dado.
+     * @return Boolean utilizzo cella
+     */
     public boolean isUsed() {
         return isUsed;
     }
 
+    /**
+     * Rimuove il dado dalla cella
+     * @throws NoDieException se la cella e' vuota
+     */
     public void removeDie() throws NoDieException {
 
         if (!isUsed())
@@ -26,12 +42,22 @@ public class Cell implements Serializable {
         isUsed = false;
     }
 
+    /**
+     * Ritorna il dado presente nella cella
+     * @return il dado della cella
+     * @throws NoDieException se la cella e' vuota
+     */
     public Die getDie() throws NoDieException {
         if (!isUsed())
             throw new NoDieException();
         return die;
     }
 
+    /**
+     * Inserisce un dado nella cella
+     * @param die dado da inserire
+     * @throws AlredySetDie se e' gia presente un dado nella cella
+     */
     public void setDie(Die die) throws AlredySetDie {
         if (isUsed)
             throw new AlredySetDie();
@@ -39,10 +65,19 @@ public class Cell implements Serializable {
         isUsed = true;
     }
 
+    /**
+     * Verifica se il dado rispetta le restrizioni della cella
+     * @param die dado da verificare
+     * @return se il dado rispetta le restrizioni
+     */
     public boolean verifyRestriction(Die die) {
         return restriction.verifyRestriction(die);
     }
 
+    /**
+     * Restituisce la restrizione presente nella cella
+     * @return la restrizione della cella
+     */
     public Restriction getRestriction() {
         return restriction;
     }
--- src/main/java/it/polimi/se2018/model/cell/Die.java
+++ src/main/java/it/polimi/se2018/model/cell/Die.java
@@ -6,35 +6,58 @@ import it.polimi.se2018.utils.NumberEnum;
 import java.io.Serializable;
 import java.util.Random;
 
-//Costruttore
+/**
+ * Classe dado
+ * @author Giampietro
+ */
 public class Die implements Serializable {
     private final Color color;  //Colore del dado; Color è un enum contenente i colori corretti
     private NumberEnum number;  //Numero del dado; NumberEnum è un enum contenente i numeri 1 a 6
     private Random random = new Random();
 
-    //Costruttore
+    /**
+     * Costruttore
+     * @param color colore del dado
+     */
     public Die(Color color) {   //Viene passato il colore al costruttore perchè un dado viene creato dalla classe DiceBag che controlla il numero esatto di dati (18 per colore)
         this.color = color; //Inizializzazione del colore passato
         number = NumberEnum.values()[random.nextInt(NumberEnum.values().length)];   //Metodo della classe Random per generare un numero casuale; NumberEnum.values().length = 6, quindi si genera un valore tra 1 a 6
     }
 
+    /**
+     * Ritorna il colore del dado
+     * @return colore del dado
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * Ritorna il numero del dado
+     * @return numero del dado
+     */
     public NumberEnum getNumber() {
         return number;
     }
 
+    /**
+     * Imposta il numero del dado
+     * @param number numero del dado
+     */
     public void setNumber(NumberEnum number) {
         this.number = number;
     }
 
-    //Generazione di un nuovo numero casuale tra 1 e 6
+    /**
+     * Ritira il dado rigenerando il valore casualmente
+     */
     public void reRoll() {
         number = NumberEnum.values()[random.nextInt(NumberEnum.values().length)];
     }
 
+    /**
+     * Gira il dado sottosopra modificando il valore del dado
+     */
     public void flip() {
         setNumber(NumberEnum.getNumber(7 - getNumber().getInt()));
     }
