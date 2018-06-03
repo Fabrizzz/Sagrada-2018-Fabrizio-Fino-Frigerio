cc060dd908ad2e3ad66a4a28afb822cdf1440fe9
giampi97 <giampi97@libero.it>
2018-06-03 19:49:55 +0200

    Aggiunta funzione StartGame()

--- src/main/java/it/polimi/se2018/controller/Controller.java
+++ src/main/java/it/polimi/se2018/controller/Controller.java
@@ -2,11 +2,20 @@ package it.polimi.se2018.controller;
 
 import it.polimi.se2018.controller.chainOfResponsibilities.Handler;
 import it.polimi.se2018.model.Model;
+import it.polimi.se2018.model.Player;
+import it.polimi.se2018.model.PlayerBoard;
+import it.polimi.se2018.objective_cards.PrivateObjective;
+import it.polimi.se2018.objective_cards.PublicObjective;
+import it.polimi.se2018.objective_cards.PublicObjectiveName;
+import it.polimi.se2018.objective_cards.public_cards.PublicObjectiveFactory;
 import it.polimi.se2018.utils.PlayerMove;
 import it.polimi.se2018.utils.enums.BoardName;
+import it.polimi.se2018.utils.enums.Color;
+import it.polimi.se2018.utils.enums.Tool;
 import it.polimi.se2018.utils.exceptions.InvalidParameterException;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Controller implements Observer {
 
@@ -31,6 +40,21 @@ public class Controller implements Observer {
             availableBoards.removeAll(boards);
         }
         return boards;
+    }
+
+    public void startGame(Map<Player, PlayerBoard> boards) {
+        List publicObjectiveNames = Arrays.asList(PublicObjectiveName.values());
+        Collections.shuffle(publicObjectiveNames);
+        List<PublicObjective> publicObjectives = new ArrayList<>();
+        publicObjectiveNames.subList(0, 3).forEach(k -> publicObjectives.add(PublicObjectiveFactory.createPublicObjective((PublicObjectiveName) k)));
+
+        List colors = Arrays.asList(Color.values());
+        Collections.shuffle(colors);
+        Iterator<Color> iterator = colors.iterator();
+        Map<Player, PrivateObjective> privateObjectiveMap = boards.keySet().stream().collect(Collectors.toMap(k -> k, t -> new PrivateObjective(iterator.next())));
+
+        this.model = new Model(new ArrayList<>(boards.keySet()), publicObjectives, boards, privateObjectiveMap, Tool.getRandTools(3));
+        setTimer(0, 0);
     }
 
     private Model getModel() {
