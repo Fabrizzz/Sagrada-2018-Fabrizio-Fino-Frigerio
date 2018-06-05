a3db841a3aa11e24b68d8f088c16da61892cedf5
MatteoF96 <matteo6.frigerio@mail.polimi.it>
2018-06-05 21:24:32 +0200

    "Aggiunte note su come scrivere la GUIProxy"

--- src/main/java/it/polimi/se2018/View/GUIProxy.java
+++ src/main/java/it/polimi/se2018/View/GUIProxy.java
@@ -1,5 +1,7 @@
 package it.polimi.se2018.View;
 
+import it.polimi.se2018.client.ClientNetwork;
+
 import java.util.Observable;
 
 /**
@@ -7,7 +9,16 @@ import java.util.Observable;
  */
 public class GUIProxy extends View {
 
-    //Da fare
+    /*
+    Da fare; bisogna utilizzare questa classe come tramite tra la GUI e il client che la istanzia.
+    In particolare deve fare da tramite per gli aspetti inziali di connessione e per i messaggi
+    relativi alle mosse fatte dall'utente. Nella classe Client occorre come dette istanziare questa
+    classe che farà da tramite.
+     */
+
+    /*public GUIProxy(){
+
+    }*/
 
 
     /**
@@ -23,5 +34,9 @@ public class GUIProxy extends View {
     @Override
     public void connectionClosed() {
 
+    }
+
+    //Potrebbe servire
+    public void createConnection(ClientNetwork clientNetwork) {
     }
 }
