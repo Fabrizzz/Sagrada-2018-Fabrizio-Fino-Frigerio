acd2fe7700b83759f9e9d2597baf2812635c64b0
Giampi97 <giampi97@libero.it>
2018-07-06 18:04:07 +0200

    Aggiunto Javadoc

--- src/main/java/it/polimi/se2018/utils/exceptions/EmptyBagException.java
+++ src/main/java/it/polimi/se2018/utils/exceptions/EmptyBagException.java
@@ -1,4 +1,7 @@
 package it.polimi.se2018.utils.exceptions;
 
+/**
+ * Exception thrown when the PlayerBoard is empty
+ */
 public class EmptyBagException extends Exception {
 }
--- src/main/java/it/polimi/se2018/utils/exceptions/FullBoardException.java
+++ src/main/java/it/polimi/se2018/utils/exceptions/FullBoardException.java
@@ -1,4 +1,7 @@
 package it.polimi.se2018.utils.exceptions;
 
+/**
+ * Exception thrown when the PlayerBoard is full
+ */
 public class FullBoardException extends Exception {
 }
